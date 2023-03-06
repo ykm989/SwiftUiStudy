fe53682e399bf095a08c1279c714fe85f39c0528
ykm989 <ykm989@naver.com>
2023-03-06 23:12:23 +0900

    하나씩 꺼내먹는 스유 강의 시작

---
+++ TodaySwiftUI/TodaySwiftUI/TodaySwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodaySwiftUIApp.swift
+//  TodaySwiftUI
+//
+//  Created by 김경호 on 2023/03/06.
+//
+
+import SwiftUI
+
+@main
+struct TodaySwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
