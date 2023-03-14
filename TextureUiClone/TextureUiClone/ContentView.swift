19fdcfac13c067e4d72ee7c4b599de4822cc5b68
ykm989 <ykm989@naver.com>
2023-03-14 20:54:51 +0900

    텍스쳐 UI 클론 코딩

---
+++ TextureUiClone/TextureUiClone/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  TextureUiClone
+//
+//  Created by 김경호 on 2023/03/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TextureUiClone/TextureUiClone/TextureUiCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextureUiCloneApp.swift
+//  TextureUiClone
+//
+//  Created by 김경호 on 2023/03/14.
+//
+
+import SwiftUI
+
+@main
+struct TextureUiCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
