de3f1fd091cda4063925cb965103b07ae86c2939
ykm989 <ykm989@naver.com>
2023-03-18 00:36:08 +0900

    Onboard contents 수정

--- TextureUiClone/TextureUiClone/Onboard.swift
+++ TextureUiClone/TextureUiClone/Onboard.swift
@@ -9,12 +9,14 @@ import SwiftUI
 
 struct Onboard: View {
     let title: String
-    let contents: String
+    let contents1: String
+    let contents2: String
+    let contents3: String
     let image: Image
     
     var title1: AttributedString{
         var result = AttributedString(title)
-        result.font = .largeTitle
+        result.font = .systemFont(ofSize: 55, weight: .bold)
         
         return result
     }
@@ -32,20 +34,42 @@ struct Onboard: View {
     
     var body: some View {
         ZStack{
+            Color("OnboardColor").edgesIgnoringSafeArea(.all)
             VStack{
+                Spacer(minLength: 100)
                 VStack{
-                    Text(title1 + title2)
                     Spacer()
-                    Text(contents)
+                    Text(title1 + title2)
+                        .frame(width: 300,alignment: .leading)
+                        
+                    Spacer(minLength: 40)
+                    VStack{
+                        Text(contents1)
+                            .frame(width: 300,alignment: .leading)
+                            .font(.system(size: 20))
+                        Text(contents2)
+                            .frame(width: 300, alignment: .leading)
+                            .font(.system(size: 20))
+                        Text(contents3)
+                            .frame(width:300 ,alignment: .leading)
+                            .font(.system(size: 20))
+                    }
                 }.frame(height:100)
+                
                 VStack{
+                    Spacer()
                     Image(systemName: "bolt")
+                        .resizable()
+                        .frame(width: 200, height: 200)
+                        .aspectRatio(contentMode: .fit)
+                    Spacer()
                     Button {
                         print("hello")
                     } label: {
                         Text("시작하기")
                     }.frame(width:200)
                         .background(.red)
+                    Spacer()
                 }
                 
             }
@@ -57,7 +81,9 @@ struct Onboard_Previews: PreviewProvider {
     static var previews: some View {
         Onboard(
             title: "Read",
-            contents: "책 속 문장으로 쉬운 독서를 시작합니다. 마음에 드는 문장은 스크랩으로 기록합니다.",
+            contents1: "책 속 문장으로 쉬운 독서를",
+            contents2: "시작합니다. 마음에 드는 문장은",
+            contents3: "스크랩으로 기록합니다.",
             image: Image(systemName: "bolt")
         )
         
