59240035ad35f5ab5069f1ac7897314b70b6016b
ykm989 <ykm989@naver.com>
2023-03-19 20:53:05 +0900

    gitignore 수정

---
+++ BasicStudy/BasicStudy/Studys/FrameStudy.swift
@@ -0,0 +1,20 @@
+//
+//  FrameStudy.swift
+//  BasicStudy
+//
+//  Created by 김경호 on 2023/03/19.
+//
+
+import SwiftUI
+
+struct FrameStudy: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FrameStudy_Previews: PreviewProvider {
+    static var previews: some View {
+        FrameStudy()
+    }
+}
