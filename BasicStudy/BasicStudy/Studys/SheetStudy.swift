d46940a0cd5c844ed87b142da79586b378332af9
ykm989 <ykm989@naver.com>
2023-03-20 22:05:33 +0900

    Sheet Study

--- BasicStudy/BasicStudy/Studys/SheetStudy.swift
+++ BasicStudy/BasicStudy/Studys/SheetStudy.swift
@@ -8,9 +8,56 @@
 import SwiftUI
 
 struct SheetStudy: View {
+    @State private var sheetOn: Bool = false
+    @State private var presentValue: Bool = false
+    
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+//            Button {
+//                presentValue.toggle()
+//            } label: {
+//                Text("화면 전환")
+//            }
+            
+            Button {
+                sheetOn.toggle()
+            } label: {
+                Text("Sheet Button")
+            }
+//            //        뷰를 불러온다.
+//            .sheet(isPresented: $sheetOn) {
+//                SecondView()
+//            }
+            // Action Sheet
+//            .actionSheet(isPresented: $sheetOn){
+//                ActionSheet(title: Text("Action Sheet"), message: Text("골라보세요."), buttons: [.default(Text("Dismiss")), .cancel(Text("Cancel"))])
+//            }
+            
+            
+            // 아래 코드는 에러가 난다. 이유는 하나의 sheet에 여러개의 뷰를 넣는것이 불가능하기 떄문이다.
+//            .sheet(isPresented: $sheetOn) {
+//                presentValue ? SecondView() : ThirdView()
+//            }
+            //        풀스크린으로 불러온다 sheet와 같은 역활
+            //        .fullScreenCover(isPresented: $sheetOn) {
+            //            SecondView()
+            //        }
+            
+            
+            // transition을 이용해서 구현 가능
+//            if sheetOn{
+//                SecondView()
+//                    .transition(AnyTransition.move(edge: .bottom))
+//                    .animation(.spring())
+//            }
+            
+//            ThirdView()
+//                .offset(y: sheetOn ? 0 : UIScreen.main.bounds.height)
+//                .animation(.spring())
+        }
     }
+    
 }
 
 struct SheetStudy_Previews: PreviewProvider {
@@ -18,3 +65,43 @@ struct SheetStudy_Previews: PreviewProvider {
         SheetStudy()
     }
 }
+
+struct SecondView: View{
+    @Environment(\.presentationMode) var presentationMode // 닫는 버튼을 가져온다.
+    
+    var body: some View{
+        ZStack{
+            Color.blue.edgesIgnoringSafeArea(.all)
+            Button {
+                self.presentationMode.wrappedValue.dismiss()
+            } label: {
+                Text("Close")
+            }
+            .frame(width:100, height: 50)
+            .background(Color.white).cornerRadius(5)
+
+        }
+    }
+}
+
+struct ThirdView: View{
+    @Environment(\.presentationMode) var presentationMode // 닫는 버튼을 가져온다.
+    
+    var body: some View{
+        ZStack{
+            Color.red.edgesIgnoringSafeArea(.all)
+            Button {
+                self.presentationMode.wrappedValue.dismiss()
+            } label: {
+                Text("Close")
+            }
+            .frame(width:100, height: 50)
+            .background(Color.white).cornerRadius(5)
+
+        }
+    }
+}
+
+/*
+ Sheet안에 조건문을 달아서 여러개의 View를 나타내기는 불가능
+ */
