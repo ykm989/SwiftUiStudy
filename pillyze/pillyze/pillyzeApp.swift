0dff8515408119f61dd4a3fcd40826317efaffa5
ykm989 <ykm989@naver.com>
2023-03-23 22:54:19 +0900

    Pillyze 프로젝트 생성

---
+++ pillyze/pillyze/pillyzeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PillyzeApp.swift
+//  Pillyze
+//
+//  Created by 김경호 on 2023/03/23.
+//
+
+import SwiftUI
+
+@main
+struct PillyzeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
