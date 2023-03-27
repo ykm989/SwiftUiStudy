305026bc027cfd02df5b457182628d7e68facc72
ykm989 <ykm989@naver.com>
2023-03-27 16:53:13 +0900

    How Many Coupe

---
+++ HMC/HMC/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  HMC
+//
+//  Created by 김경호 on 2023/03/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HMC/HMC/HMCApp.swift
@@ -0,0 +1,17 @@
+//
+//  HMCApp.swift
+//  HMC
+//
+//  Created by 김경호 on 2023/03/27.
+//
+
+import SwiftUI
+
+@main
+struct HMCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
